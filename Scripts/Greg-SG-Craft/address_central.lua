7efb89e2a9bded6147958be9389755172500eeb1
Konrad-Wszelaki <konwszelaki@gmail.com>
2025-10-26 18:49:46 +0100

    Revert "Added autoupdate functionality"
    This reverts commit ffb9566eb2d18ba693be65302003b2d3aaf23c78.

--- Scripts/Greg-SG-Craft/address_central.lua
+++ Scripts/Greg-SG-Craft/address_central.lua
@@ -1,30 +1,15 @@
-local version_major = "2"
+local version_major = "1"
 local version_minor = "0"
 
 local component     = require("component")
 local serialization = require("serialization")
 local event         = require("event")
 
-local kwlib = require("KWLib")
-local KWLIB_VERSION_MAJOR = "2"
-local KWLIB_VERSION_MINOR = "3"
-io.write("checking KWLib version compat: ")
-if not kwlib.checkVersionCompat(kwlib, KWLIB_VERSION_MAJOR, KWLIB_VERSION_MINOR) then
-    error("KWLib library version incompatible, cannot proceed...")
-end
-io.write("version compatible...\n")
-
 local network_card = component.modem
 local paired_card  = component.tunnel
 
 local NETWORK_COMMS_PORT = 13
 network_card.open(NETWORK_COMMS_PORT)
-
--- network card message types
-local net_message_types = {
-    "new_address"   = "new_address",
-    "update_request"= "update_request"
-}
 
 local run = true
 
@@ -39,7 +24,6 @@ local function handleLocalNetworkMessage(localAddress, remoteAddress, port, dist
     end
     --print("\n")
     paired_card.send(serializedPacket)
-    if data["TYPE"] == net_message_types["update_request"] then kwlib.general.updateAllPackages(kwlib, kwlib.strings.toBoolean[data["restart"]]) end
 end
 
 local function handlePairedCardMessage(localAddress, remoteAddress, port, distance, serializedPacket, ...)
